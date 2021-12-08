f126cdf2a85c30477196ac59571d4bc8a90eb409
Gabriel Gianni <contatogabrielgianni@gmail.com>
2021-12-08 16:32:12 -0300

    :tada: first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contato</title>
+</head>
+<body class="antialiased">
+    <h1>Contato</h1>
+    <a href="/">Voltar para o início</a>
+</body>
+</html>
